67dcbadce0d14908e5309396dae844cf20ca40b5
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-04-01 11:01:04 +0900

    없는숫자더하기

---
+++ 3페이지 level 1 13번 - 없는숫자더하기/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3페이지_level_1_13번___없는숫자더하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1,2,3,4,5,6,7,8};
+            int answer = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                answer += i;
+            }
+
+            foreach(var item in numbers)
+            {
+                answer -= item;
+            }
+            Console.WriteLine("없는 숫자를 더한 값은 => " + answer);
+        }
+    }
+}
