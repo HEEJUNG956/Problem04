caad68605636fd580c21c5722f4a7cef84974606
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-04-04 09:07:03 +0900

    체육복

---
+++ 3페이지 level 1 07번 - 체육복/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3페이지_level_1_07번___체육복
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DD");
+
+        }
+    }
+}
