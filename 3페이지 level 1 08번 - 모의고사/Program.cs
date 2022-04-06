3681f88f0cfbdbac6de417400ae24b9dac61fbeb
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-04-06 16:09:11 +0900

    최종

--- 3페이지 level 1 08번 - 모의고사/Program.cs
+++ 3페이지 level 1 08번 - 모의고사/Program.cs
@@ -14,6 +14,8 @@ namespace _3페이지_level_1_08번___모의고사
             string answer1 = Console.ReadLine();
             int answer2 = Convert.ToInt32(answer1);
             int[] answers = new int[answer2];
+
+
             for(int i = 0; i < answer2; i++)
             {
                 Console.Write(i + 1 + "번째 답 입력하기 => ");
@@ -22,10 +24,12 @@ namespace _3페이지_level_1_08번___모의고사
                 answers[i] = answer4;
             }
 
+
             int[] student1 = new int[] { 1, 2, 3, 4, 5 };
             int[] student2 = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
             int[] student3 = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
             int[] temp = new int[3];
+
 
             for(int i = 0; i < answers.Length; i++)
             {
@@ -34,10 +38,12 @@ namespace _3페이지_level_1_08번___모의고사
                 if (answers[i] == student3[i % student3.Length]) ++temp[2];
             }
 
+
             List<int> answer = new List<int>();
             if(temp[0] == temp.Max()) answer.Add(1);
             if(temp[1] == temp.Min()) answer.Add(2);
             if(temp[2] == temp.Max()) answer.Add(3);
+
 
             
         }
